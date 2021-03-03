b93b6ebb68ae27eac26b30906b25a923d05a9c32
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-03-03 20:15:37 -0300

    Adicionando EndPoints no Controller de Ecommerce

--- aplication/api/src/main/java/br/com/grupo8/api/Controllers/EcommerceController.java
+++ aplication/api/src/main/java/br/com/grupo8/api/Controllers/EcommerceController.java
@@ -3,12 +3,49 @@ package br.com.grupo8.api.Controllers;
 import br.com.grupo8.api.models.Ecommerce;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ecommercies")
 public class EcommerceController {
 
+    private List<Ecommerce> ecommerces = new ArrayList<Ecommerce>();
+
     @PostMapping
-    public String cadastrarEcommerc(@RequestBody Ecommerce ecommerce){
-        
+    public String cadastrarEcommerce(@RequestBody Ecommerce ecommerce){
+        if(ecommerce.getCnpj() == null || ecommerce.getNome() == null){
+            return "Valores inválidos";
+        }
+
+        ecommerces.add(ecommerce);
+        return "Ecommerc cadastrado! :)";
+    }
+
+    @GetMapping
+    public List<Ecommerce> getEcommerces() {
+        return ecommerces;
+    }
+
+    @GetMapping("/{idEcommerce}")
+    public Ecommerce getEcommerce(@PathVariable Integer idEcommerce) {
+        if (ecommerces.size() > idEcommerce){
+            return ecommerces.get(idEcommerce);
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/{idEcommerce}")
+    public String deleteEcommerce(@PathVariable int idEcommerce){
+        ecommerces.remove(idEcommerce);
+        return "Ecommerce removido com sucesso";
+    }
+
+    @PutMapping("/{idEcommerce}")
+    public String atualizarEcommerce(@RequestBody Ecommerce ecommerce,
+                                     @PathVariable Integer idEcommerce){
+        ecommerces.set(idEcommerce, ecommerce);
+        return "Ecommerce atualizado com sucesso";
     }
 }
