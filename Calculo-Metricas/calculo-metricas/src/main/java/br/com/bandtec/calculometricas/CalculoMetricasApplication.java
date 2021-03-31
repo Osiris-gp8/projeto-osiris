4e32d7df2024ff6f7b8de3b1bbdb8874543779f1
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-03-31 14:43:00 -0300

    Ambiente de construção da API de métricas

---
+++ Calculo-Metricas/calculo-metricas/src/main/java/br/com/bandtec/calculometricas/CalculoMetricasApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.calculometricas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoMetricasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoMetricasApplication.class, args);
+	}
+
+}
