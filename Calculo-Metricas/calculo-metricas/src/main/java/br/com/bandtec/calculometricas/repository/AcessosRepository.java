2a1ed61f28cdff9f99ed5c31afeebcec9842506b
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-03-31 18:00:14 -0300

    Criação das repository's com querys customizadas

---
+++ Calculo-Metricas/calculo-metricas/src/main/java/br/com/bandtec/calculometricas/repository/AcessosRepository.java
@@ -0,0 +1,11 @@
+package br.com.bandtec.calculometricas.repository;
+
+import br.com.bandtec.calculometricas.model.Acessos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface AcessosRepository extends JpaRepository<Acessos, Integer> {
+    @Query(value = "select count(id_acessos) as quantidade from acessos where inicio_acesso " +
+            "between current_date()-7 and current_date()", nativeQuery = true)
+    Integer countAcessosSemana();
+}
