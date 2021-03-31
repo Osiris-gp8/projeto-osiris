6bacc0158c2e131a7534cf7b50a6c709dee420a1
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-03-31 14:33:04 -0300

    Substituição do projeto

--- Calculo-Metricas/src/main/java/br/com/bandtec/calculometricas/repository/CupomRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.bandtec.calculometricas.repository;
-
-import br.com.bandtec.calculometricas.model.dao.Cupom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CupomRepository extends JpaRepository<Cupom, Integer> {
-}
--- Calculo-Metricas/src/main/java/br/com/bandtec/calculometricas/repository/DominioStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.bandtec.calculometricas.repository;
-
-import br.com.bandtec.calculometricas.model.dao.DominioStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DominioStatusRepository extends JpaRepository<DominioStatus, Integer> {
-}
