a403892c9a072ea1f91a97d976627ea88c6e9a6e
igor-goes <igor.nascimento@bandtec.com.br>
2021-04-03 23:28:52 -0300

    Atualização script banco

--- Calculo-Metricas/script-banco.sql
+++ Calculo-Metricas/script-banco.sql
@@ -10,8 +10,8 @@ create table if not exists cupom (
 	id_cupom int primary key auto_increment,
     nome_cupom varchar(45),
     valor decimal(2,1),
-    data_expirado date,
-    data_validado date
+    data_expirado datetime,
+    data_validado datetime
 );
 
 create table if not exists ecommerce (
@@ -20,13 +20,6 @@ create table if not exists ecommerce (
     nome varchar(45)
 );
 
-create table if not exists usuario (
-	id_usuario int primary key auto_increment,
-    login varchar(45),
-    senha varchar(45),
-    fk_ecommerce int,
-    foreign key (fk_ecommerce) references ecommerce(id_ecommerce)
-);
 
 create table if not exists evento (
 	id_compra int primary key auto_increment,
@@ -34,7 +27,7 @@ create table if not exists evento (
     nome_produto varchar(45),
     preco decimal(6,2),
     nome_categoria varchar(45),
-    data_compra date,
+    data_compra datetime,
     cupom varchar(45),
     fk_ecommerce int,
     fk_cupom int,
@@ -44,11 +37,20 @@ create table if not exists evento (
     foreign key (fk_status) references dominio_status(id_dominio_status)
 );
 
+
+create table if not exists usuario (
+	id_usuario int primary key auto_increment,
+    login varchar(45),
+    senha varchar(45),
+    fk_ecommerce int,
+    foreign key (fk_ecommerce) references ecommerce(id_ecommerce)
+);
+
 create table if not exists acessos (
 	id_acessos int primary key auto_increment,
 	id_consumidorEcommerce int,
-	inicio_acesso date,
-	fim_acesso date
+	inicio_acesso datetime,
+	fim_acesso datetime
 );
 
 insert into dominio_status values
@@ -56,10 +58,10 @@ insert into dominio_status values
 (null, "Finalizada");
 
 insert into cupom values
-(null, "", 0.0, "2021-03-31", "2021-03-26"),
-(null, "OSIRIS10", 0.1, "2021-03-31", "2021-03-26"),
-(null, "OSIRIS20", 0.2, "2021-04-10", "2021-04-05"),
-(null, "OSIRIS50", 0.5, "2021-04-15", "2021-04-10");
+(null, "", 0.0, "2021-03-31 00:00:01", "2021-03-26 23:59:59"),
+(null, "OSIRIS10", 0.1, "2021-01-31 00:00:01", "2021-03-26 23:59:59"),
+(null, "OSIRIS20", 0.2, "2021-02-10 00:00:01", "2021-04-05 23:59:59"),
+(null, "OSIRIS50", 0.5, "2021-02-15 00:00:01", "2021-04-10 23:59:59");
 
 insert into ecommerce values
 (null, "01234567891234", "ficticio");
@@ -68,19 +70,23 @@ insert into usuario values
 (null, "zezinho", "12345", 1);
 
 insert into evento values
-(null, 505, "Calça jeans", 70.89, "Roupas", "2021-03-26", "", 1, 1, 1),
-(null, 505, "Camiseta", 70.89, "Roupas", "2021-03-26", "OSIRIS20", 1, 2, 2),
-(null, 505, "Mouse gamer", 70.89, "Periféricos", "2021-03-26", "CASCA10", 1, 1, 2),
-(null, 203, "Teclado USB", 70.89, "Periféricos", "2021-03-26", "CASCA10", 1, 1, 2),
-(null, 203, "Teclado USB", 70.89, "Periféricos", "2021-03-26", "CASCA10", 1, 1, 2);
+(null, 505, "Calça jeans", 70.89, "Roupas", "2021-03-26 23:00:00", "", 1, 1, 1),
+(null, 505, "Camiseta", 70.89, "Roupas", "2021-03-26 21:25:34", "OSIRIS20", 1, 2, 2),
+(null, 505, "Mouse gamer", 70.89, "Periféricos", "2021-03-26 22:43:43", "CASCA10", 1, 1, 2),
+(null, 203, "Teclado USB", 70.89, "Periféricos", "2021-03-26 12:23:54", "CASCA10", 1, 1, 2),
+(null, 203, "Teclado USB", 70.89, "Periféricos", "2021-03-26 13:00:00", "CASCA10", 1, 1, 2);
 
 insert into acessos values
-(null, 505, "2021-03-26", "2021-03-27"),
-(null, 203, "2021-03-27", "2021-03-28");
+(null, 505, "2021-03-25 21:15:23", "2021-03-26 23:20:15"),
+(null, 203, "2021-03-27 12:12:12", "2021-03-27 13:00:45");
 
-select * from evento where id_consumidor_ecommerce = 203 group by nome_produto;
+select * from evento,acessos where id_consumidor_ecommerce = 505;
 
-select count(id_acessos) as quantidade from acessos where inicio_acesso between current_date()-7 and current_date();
+select id_consumidor_ecommerce,nome_categoria from evento;
+
+select * from evento;
+
+select count(id_acessos) as quantidade from acessos where inicio_acesso between current_datetime()-7 and current_datetime();
 select count(id_compra) as quantidade from evento;
 select count(id_acessos) as quantidade from acessos;
 select nome_categoria, count(id_compra) as quantidade from evento group by nome_categoria order by quantidade desc limit 5;
