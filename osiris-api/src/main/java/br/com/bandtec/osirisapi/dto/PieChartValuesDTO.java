44afc84b0cc6c5c2e9298d733c6535f6deda7f4b
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-04-22 19:47:23 -0300

    Renomeando packages e classes

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/dto/PieChartValuesDTO.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/dto/PieChartValuesDTO.java
@@ -1,4 +1,4 @@
-package br.com.bandtec.osirisapi.data;
+package br.com.bandtec.osirisapi.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class PieChartValues {
+public class PieChartValuesDTO {
 
     private String label;
     private Double value;
--- osiris-api/src/main/java/br/com/bandtec/osirisapi/views/CupomMaisUsadoView.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/views/CupomMaisUsadoView.java
@@ -2,7 +2,7 @@ package br.com.bandtec.osirisapi.views;
 
 import org.springframework.beans.factory.annotation.Value;
 
-public interface CupomMaisUsado {
+public interface CupomMaisUsadoView {
 
     @Value("#{target.nome}")
     String getNome();
