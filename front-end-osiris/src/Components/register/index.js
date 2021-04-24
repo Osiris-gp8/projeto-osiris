409e3b44e8c06c33b73a54e9eb1f59df19f15def
daniel-zacarias <daniel.santos@bandtec.com.br>
2021-04-24 19:09:15 -0300

    Cadastro finalizado

---
+++ front-end-osiris/src/Components/register/index.js
@@ -0,0 +1,63 @@
+import { React, useState } from 'react';
+import leftblob from '../../Images/left-blob.svg'
+import rightblob from '../../Images/right-blob.svg'
+import { Button, Container, ContainerForm, Form , RadioBox, RadioButton} from './style';
+import { Link } from 'react-router-dom';
+
+
+export default () => {
+    const [next, setNext]= useState(false);
+
+    function changeForm(e){
+        setNext(e.target.id != 'first')
+    }
+
+    return (
+        <Container>
+            <img src={leftblob} alt="React Logo" />
+            <Form>
+                <ContainerForm next={!next}>
+                    <h2>Cadastro</h2>
+                    <div>
+                        <label>CNPJ</label>
+                        <input  placeholder='00.000.000/0000-00'/>
+                    </div>
+                    <div>
+                        <label>Usuário</label>
+                        <input/>
+                    </div>
+                    <div>
+                        <label>Nome Completo</label>
+                        <input/>
+                    </div>
+                    <Link to="/">Voltar para tela de Login</Link>
+                </ContainerForm>
+                <ContainerForm   next={next}>
+                    <h2>Cadastro</h2>
+                    <div>
+                        <label>Nome do Comércio</label>
+                        <input/>
+                    </div>
+                    <div>
+                        <label>Senha</label>
+                        <input type='password'/>
+                    </div>
+                    <div>
+                        <label>Confirmar senha</label>
+                        <input type='password'/>
+                    </div>
+                    <Link to="/">Voltar para tela de Login</Link>
+                </ContainerForm>
+                <RadioBox>
+                    <input type="radio" name='radio' defaultChecked
+                    checked={!next}/>
+                    <RadioButton id='first'onClick={changeForm}/>
+                    <input type="radio" name='radio' checked={next}/>
+                    <RadioButton onClick={changeForm}/>
+                </RadioBox>
+                <Button>Enviar</Button>
+            </Form>
+            <img src={rightblob} alt="React Logo" />
+        </Container>
+    );
+};
---
+++ front-end-osiris/src/Components/register/style.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+import { Container as Cont, ContainerForm as CF
+, Form as FR, Button as BTN} from '../login/style'
+
+
+export const Container = styled(Cont)``
+
+export const Form = styled(FR)`
+    
+    height:100vh;
+`
+
+export const RadioBox = styled.div`
+    align-self:center;
+    width:20%;
+    display:flex;
+    justify-content:space-around;
+    input{
+        opacity: 0;
+        display:none;
+    }
+
+    input:nth-of-type(1):checked ~ div:nth-of-type(1){
+        background-color:#666BC2;
+    }
+
+    input:nth-of-type(2):checked ~ div:nth-of-type(2){
+        background-color:#666BC2;
+    }
+
+`
+export const RadioButton = styled.div`
+    content:'';
+    /* position: absolute; */
+    height: 25px;
+    width: 25px;
+    background-color: #BBB;
+    border-radius: 100%;
+    cursor: pointer;
+`
+
+export const ContainerForm = styled(CF)`
+    display:${props =>  !props.next? 'none':'flex'}
+`
+
+export const Button = styled(BTN)`
+
+
+`
+
