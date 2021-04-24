302db3ac5a07a03856fb8c95fa2d41a012b48bae
daniel-zacarias <daniel.santos@bandtec.com.br>
2021-04-24 19:11:06 -0300

    Pages finalizadas

---
+++ front-end-osiris/src/Pages/Relation.js
@@ -0,0 +1,12 @@
+import Menu from '../Components/Menu'
+import Relation from '../Components/relation'
+import Suport_flex from '../Components/Style-Suport/Suport_flex'
+
+export default () =>{ 
+return (
+    <Suport_flex>
+        <Menu/>
+        <Relation/>
+    </Suport_flex>
+)
+}
