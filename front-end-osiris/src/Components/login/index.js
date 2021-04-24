67655438631373442eb3f1beb13412f9a6c178ef
daniel-zacarias <daniel.santos@bandtec.com.br>
2021-04-24 19:08:48 -0300

    Login finalizado

---
+++ front-end-osiris/src/Components/login/index.js
@@ -0,0 +1,51 @@
+import {React, useState} from 'react';
+import leftblob from '../../Images/left-blob.svg'
+import rightblob from '../../Images/right-blob.svg'
+import { Container, Form, ContainerForm, Button} from './style';
+import {Link,useHistory} from 'react-router-dom';
+
+
+
+export default () => {
+    const [cnpj, setCNPJ] = useState('');
+    const [senha, setSenha] = useState('');
+    const history = useHistory()
+
+    function onSubmit(e){
+        e.preventDefault()
+        if(cnpj == '00.000.000/0000-00' && senha == '1234'){
+            return history.push('/home')
+        }
+    }
+
+
+    return (
+    <Container>
+        <img src={leftblob} alt="Blob a esquerda" />
+            <Form onSubmit={onSubmit}>
+            <ContainerForm>
+                    <h2>Login</h2>
+                    <div>
+                        <label for='cnpj'>CNPJ/Usuário</label>
+                        <input id='cnpj' value={cnpj} 
+                        onChange={(e) => setCNPJ(e.target.value)} 
+                        placeholder='00.000.000/0000-00'/>
+                    </div>
+                    <div>
+                        <label for='senha'>Senha</label>
+                        <input
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                        id='senha' type='password'/>
+                    </div>
+                    <div>
+                        <Link>Esqueceu sua senha?</Link>
+                        <Link to="/register">Não possui cadastro?</Link>
+                    </div>
+                </ContainerForm>
+                <Button>Logar</Button>
+            </Form>
+        <img src={rightblob} alt="Blov a direita" />
+    </Container>
+    );
+};
