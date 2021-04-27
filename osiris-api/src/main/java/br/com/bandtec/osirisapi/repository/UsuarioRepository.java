6951e546e85ab9655f53423fefb9f76c3825e580
patrick_lessa <patrick.lessa03@outlook.com>
2021-04-26 21:28:05 -0300

    usuarioRpository dando erro na query

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/repository/UsuarioRepository.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/repository/UsuarioRepository.java
@@ -2,7 +2,11 @@ package br.com.bandtec.osirisapi.repository;
 
 import br.com.bandtec.osirisapi.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
-}
+    @Query(value = "select * from usuario where login = ?1 and senha = ?2", nativeQuery = true)
+    List<Usuario> findByLoginEqualsAndSenhaEquals(String login, String senha);
