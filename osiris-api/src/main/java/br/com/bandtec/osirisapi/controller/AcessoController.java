d61397526fe6421b5b894fd938012eb30b25c7f0
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-05-03 09:28:58 -0300

    Criando AcessoService e refatorando AcessoController

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/controller/AcessoController.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/controller/AcessoController.java
@@ -2,6 +2,8 @@ package br.com.bandtec.osirisapi.controller;
 
 import br.com.bandtec.osirisapi.domain.Acesso;
 import br.com.bandtec.osirisapi.repository.AcessoRepository;
+import br.com.bandtec.osirisapi.service.AcessoService;
+import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,21 +15,16 @@ import java.util.List;
 @AllArgsConstructor
 public class AcessoController {
 
-    private final AcessoRepository acessoRepository;
+    private final AcessoService acessoService;
 
     @GetMapping
-    public ResponseEntity getAcessos() {
-        List<Acesso> acessos = acessoRepository.findAll();
-        if (acessos.isEmpty()) {
-            return ResponseEntity.status(204).build();
-        } else {
-            return ResponseEntity.status(200).body(acessos);
-        }
+    public ResponseEntity getAcessos() throws NotFoundException {
+        return ResponseEntity.status(200).body(acessoService.getAcessos());
     }
 
     @PostMapping
     public ResponseEntity postAcesso(@RequestBody Acesso novoAcesso) {
-        acessoRepository.save(novoAcesso);
+        acessoService.inserirAcesso(novoAcesso);
         return ResponseEntity.status(201).build();
     }
 
---
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/service/AcessoService.java
@@ -0,0 +1,30 @@
+package br.com.bandtec.osirisapi.service;
+
+import br.com.bandtec.osirisapi.domain.Acesso;
+import br.com.bandtec.osirisapi.repository.AcessoRepository;
+import javassist.NotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class AcessoService {
+
+    private final AcessoRepository acessoRepository;
+
+    public List<Acesso> getAcessos() throws NotFoundException {
+        List<Acesso> acessos = acessoRepository.findAll();
+        if (acessos.isEmpty()) {
+            throw new NotFoundException("Não existem acessos");
+        }
+
+        return acessos;
+    }
+
+    public void inserirAcesso(Acesso novoAcesso) {
+        acessoRepository.save(novoAcesso);
+    }
+}
