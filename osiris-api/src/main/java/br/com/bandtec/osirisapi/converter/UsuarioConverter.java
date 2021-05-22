77bb7ccfabf14c492b4186cedfb0ae8f4f7a19cd
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-05-22 02:45:12 -0300

    Adicionando usuarioListToUsuarioResponseList

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/converter/UsuarioConverter.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/converter/UsuarioConverter.java
@@ -3,6 +3,9 @@ package br.com.bandtec.osirisapi.converter;
 import br.com.bandtec.osirisapi.domain.Usuario;
 import br.com.bandtec.osirisapi.dto.response.UsuarioResponse;
 
+import java.util.List;
+
 public interface UsuarioConverter {
-    public UsuarioResponse usuarioToUsuarioResponse(Usuario usuario);
+    UsuarioResponse usuarioToUsuarioResponse(Usuario usuario);
+    List<UsuarioResponse> usuarioListToUsuarioResponseList(List<Usuario> usuarios);
 }
--- osiris-api/src/main/java/br/com/bandtec/osirisapi/converter/implementation/UsuarioConverterImplementation.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/converter/implementation/UsuarioConverterImplementation.java
@@ -4,6 +4,9 @@ import br.com.bandtec.osirisapi.converter.UsuarioConverter;
 import br.com.bandtec.osirisapi.domain.Usuario;
 import br.com.bandtec.osirisapi.dto.response.UsuarioResponse;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsuarioConverterImplementation implements UsuarioConverter {
 
     @Override
@@ -14,5 +17,13 @@ public class UsuarioConverterImplementation implements UsuarioConverter {
                 .nomeCompleto(usuario.getNomeCompleto())
                 .ecommerce(usuario.getEcommerce())
                 .build();
+    }
+
+    @Override
+    public List<UsuarioResponse> usuarioListToUsuarioResponseList(List<Usuario> usuarios) {
+        return usuarios
+                .stream()
+                .map(usuario -> usuarioToUsuarioResponse(usuario))
+                .collect(Collectors.toList());
     }
 }
