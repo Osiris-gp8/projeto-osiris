8176ca966962eaf9c4850ac6da4eb0e6a4ab3da4
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-05-22 02:39:52 -0300

    Refatorando UsuarioResponse

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/dto/response/UsuarioResponse.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/dto/response/UsuarioResponse.java
@@ -1,12 +1,14 @@
 package br.com.bandtec.osirisapi.dto.response;
 
 import br.com.bandtec.osirisapi.domain.Ecommerce;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.ManyToOne;
 
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
