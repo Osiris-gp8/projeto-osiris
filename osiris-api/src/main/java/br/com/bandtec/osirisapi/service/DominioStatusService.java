abacf812a67b790658cb939cdffba6eb33a36a90
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-05-22 22:18:32 -0300

    Refatorando retorno de erros DominioStatusService

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/service/DominioStatusService.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/service/DominioStatusService.java
@@ -1,6 +1,7 @@
 package br.com.bandtec.osirisapi.service;
 
 import br.com.bandtec.osirisapi.domain.DominioStatus;
+import br.com.bandtec.osirisapi.exception.ApiRequestException;
 import br.com.bandtec.osirisapi.repository.DominioStatusRepository;
 import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
@@ -18,41 +19,41 @@ public class DominioStatusService {
 
     private final DominioStatusRepository dominioStatusRepository;
 
-    public List<DominioStatus> getAllDominios() throws NotFoundException {
+    public List<DominioStatus> getAllDominios() {
         List<DominioStatus> dominios = dominioStatusRepository.findAll();
         if (dominios.isEmpty()) {
-            throw new NotFoundException("Não existem dominios");
+            throw new ApiRequestException("Não existem dominios", HttpStatus.NO_CONTENT);
         } else {
             return dominios;
         }
     }
 
-    public void saveDominio(DominioStatus dominioStatus) throws HttpClientErrorException {
+    public void saveDominio(DominioStatus dominioStatus) {
         Optional<DominioStatus> dominioStatusOptional = dominioStatusRepository.findOneByNome(dominioStatus.getNome());
         if (dominioStatusOptional.isPresent()){
 
             if (dominioStatusOptional.get().getNome().equals(dominioStatus.getNome())){
 
-                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Dominio já existente");
+                throw new ApiRequestException("Dominio já existente", HttpStatus.BAD_REQUEST);
             }
         }
 
         dominioStatusRepository.save(dominioStatus);
     }
 
-    public void deleteDomino(int idDominioStatus) throws NotFoundException {
+    public void deleteDomino(int idDominioStatus) {
         if (!dominioStatusRepository.existsById(idDominioStatus)) {
-            throw new NotFoundException("Dominio não existente");
+            throw new ApiRequestException("Dominio não existente", HttpStatus.BAD_REQUEST);
         }
         dominioStatusRepository.deleteById(idDominioStatus);
     }
 
-    public void atualizarDominioPeloId(Integer idDominioStatus, DominioStatus dominioStatus) throws NotFoundException {
+    public void atualizarDominioPeloId(Integer idDominioStatus, DominioStatus dominioStatus) {
 
         Optional<DominioStatus> dominioStatusOptional = dominioStatusRepository.findById(idDominioStatus);
 
         if (!dominioStatusOptional.isPresent()){
-            throw new NotFoundException("Dominio não existente");
+            throw new ApiRequestException("Dominio não existente", HttpStatus.BAD_REQUEST);
         }
         DominioStatus dominioStatusParaAtualizar = dominioStatusOptional.get();
 
