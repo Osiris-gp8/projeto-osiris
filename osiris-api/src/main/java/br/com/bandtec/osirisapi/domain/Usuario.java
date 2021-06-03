8e0cb3e067236c2327f9a087e0d469effc6745da
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-06-03 17:18:52 -0300

    Adicionando métodos de configuração do Spring Security

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Usuario.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Usuario.java
@@ -3,22 +3,26 @@ package br.com.bandtec.osirisapi.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idUsuario;
 
     @NotBlank
-    private String login;
+    private String loginUsuario;
 
     @NotBlank
     private String senha;
@@ -27,4 +31,42 @@ public class Usuario {
     @Positive
     @ManyToOne
     private Ecommerce ecommerce;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Perfil> perfis = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.perfis;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.loginUsuario;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
