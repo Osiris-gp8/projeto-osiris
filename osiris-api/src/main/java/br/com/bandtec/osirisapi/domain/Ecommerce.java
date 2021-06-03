5da4535a2bc1513ab20ea086d64a7fcf1ed6dba3
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-06-03 17:15:57 -0300

    Colocando asterisco no import

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Ecommerce.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Ecommerce.java
@@ -4,11 +4,7 @@ package br.com.bandtec.osirisapi.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -27,5 +23,4 @@ public class Ecommerce {
 
     @NotBlank
     private String nome;
-
 }
