327437647f9aef83747a2010782db0e2aea647f8
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-06-03 17:20:35 -0300

    Adicionando método de autenticação do usuário

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/dto/UsuarioAcessoRequest.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/dto/UsuarioAcessoRequest.java
@@ -2,12 +2,18 @@ package br.com.bandtec.osirisapi.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
 public class UsuarioAcessoRequest {
 
     private String login;
     private String senha;
 
+    public UsernamePasswordAuthenticationToken converter() {
+        return new UsernamePasswordAuthenticationToken(login, senha);
+    }
 }
--- osiris-api/src/main/java/br/com/bandtec/osirisapi/repository/UsuarioRepository.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/repository/UsuarioRepository.java
@@ -10,4 +10,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     @Query(value = "select * from usuario where login = ?1 and senha = ?2", nativeQuery = true)
     Optional<Usuario> findByLoginEqualsAndSenhaEquals(String login, String senha);
+
+    Optional<Usuario> findByLogin(String Login);
 }
