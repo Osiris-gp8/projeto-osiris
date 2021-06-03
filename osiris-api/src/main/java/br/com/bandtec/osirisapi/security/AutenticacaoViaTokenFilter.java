0c3cb245b1f1970e3fb8e367898c6a09d8876c0f
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-06-03 17:28:06 -0300

    Filtro de autenticação

---
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,27 @@
+package br.com.bandtec.osirisapi.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recuperarToken(request);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            return null;
+        }
+        return token.substring(7, token.length());
+    }
+}
