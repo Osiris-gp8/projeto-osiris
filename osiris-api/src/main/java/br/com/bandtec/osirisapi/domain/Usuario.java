a25af709319a8b89a71e0977a01e8343a2308b16
patrick_lessa <patrick.lessa03@outlook.com>
2021-06-08 18:25:55 -0300

    add: validation sizes na model de usuário e ecommerce

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Usuario.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Usuario.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -22,12 +23,15 @@ public class Usuario implements UserDetails {
     private Integer idUsuario;
 
     @NotBlank
+    @Size(min = 5, max = 45)
     private String nomeCompleto;
 
     @NotBlank
+    @Size(min = 4, max = 8)
     private String loginUsuario;
 
     @NotBlank
+    @Size(min = 8, max = 16)
     private String senha;
 
     @NotNull
