8b0574663e73e6422e100a710d518908175c53b0
Kaio Silva Baleeiro de Jesus <kaio.jesus@safra.com.br>
2021-06-09 18:41:16 -0300

    Criando os testes da classe Ecommerce

---
+++ osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/EcommerceControllerTest.java
@@ -0,0 +1,62 @@
+package br.com.bandtec.osirisapi.controller;
+
+import br.com.bandtec.osirisapi.domain.Ecommerce;
+import br.com.bandtec.osirisapi.repository.EcommerceRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EcommerceControllerTest {
+
+    @Autowired
+    EcommerceController ecommerceController;
+
+    @MockBean
+    EcommerceRepository ecommerceRepository;
+
+    @Test
+    void getEcommerce() {
+        List<Ecommerce> ecommerceList =
+                Arrays.asList(new Ecommerce(), new Ecommerce(), new Ecommerce());
+
+        Mockito.when(ecommerceRepository.findAll()).thenReturn(ecommerceList);
+        ResponseEntity<List<Ecommerce>> resposta = ecommerceController.getEcommerce();
+    }
+
+    @Test
+    void postEcommerce() {
+        Ecommerce ecommerce = new Ecommerce();
+
+        ResponseEntity resposta = ecommerceController.postEcommerce(ecommerce);
+
+        assertEquals(201, resposta.getStatusCodeValue());
+    }
+
+    @Test
+    void deleteEcommerce() {
+        int idTeste = 1;
+
+        Mockito.when(ecommerceRepository.existsById(idTeste))
+                .thenReturn(true);
+
+        ResponseEntity resposta =
+                ecommerceController.deleteEcommerce(idTeste);
+
+        assertEquals(200, resposta.getStatusCodeValue());
+    }
+
+    @Test
+    void atualizarEcommerce() {
+        int idTeste = 1;
+
+    }
+}
