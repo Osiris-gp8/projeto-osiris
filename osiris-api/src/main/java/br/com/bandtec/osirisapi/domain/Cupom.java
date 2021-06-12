e38135a98d74264f1ac70ccd7e026108d63e1afd
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-06-12 16:44:26 -0300

    feat: new field id_ecommerce in acesso and cupom

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Cupom.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/domain/Cupom.java
@@ -2,10 +2,7 @@ package br.com.bandtec.osirisapi.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
@@ -44,4 +41,8 @@ public class Cupom {
 
     @NotNull
     private Boolean usado;
+
+    @NotNull
+    @ManyToOne
+    private Ecommerce ecommerce;
 }
