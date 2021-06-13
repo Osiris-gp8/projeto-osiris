bb1f7a8bcc7edf831739476657949dc3bbfad53e
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-06-13 17:04:20 -0300

    chore: ajust other in spring security cors

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/security/SecurityConfigurations.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/security/SecurityConfigurations.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +26,7 @@ import java.util.Collections;
 @AllArgsConstructor
 @EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     private final AutenticacaoService autenticacaoService;
