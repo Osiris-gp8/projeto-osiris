6d121d9d8b00d1eb4fef39e14a85b828e5da9096
Kaio Silva Baleeiro de Jesus <kaio.jesus@safra.com.br>
2021-06-14 07:17:47 -0300

    Testes dos usuários

--- osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/UsuarioControllerTest.java
+++ osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/UsuarioControllerTest.java
@@ -2,14 +2,18 @@ package br.com.bandtec.osirisapi.controller;
 
 import br.com.bandtec.osirisapi.domain.Ecommerce;
 import br.com.bandtec.osirisapi.domain.Usuario;
+import br.com.bandtec.osirisapi.exception.ApiRequestException;
 import br.com.bandtec.osirisapi.repository.EcommerceRepository;
 import br.com.bandtec.osirisapi.repository.UsuarioRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -28,7 +32,8 @@ class UsuarioControllerTest {
     EcommerceRepository ecommerceRepository;
 
     @Test
-    void getUsuario() {
+    @DisplayName("GET / - Quando há usuários na base")
+    void getUsuarioOk() {
         List<Usuario> usuarioList =
                 Arrays.asList(new Usuario(), new Usuario(), new Usuario());
 
@@ -39,6 +44,19 @@ class UsuarioControllerTest {
     }
 
     @Test
+    @DisplayName("GET / - Quando há usuários na base")
+    void getUsuarioNaoOk() {
+        Mockito.when(usuarioRepository.findAll()).thenReturn(new ArrayList<>());
+
+        try {
+            ResponseEntity<List<Usuario>> resposta = usuarioController.getUsuario();
+        } catch (ApiRequestException e) {
+            assertEquals(204, e.getStatus().value());
+        }
+    }
+
+    @Test
+    @DisplayName("POST / - Cadastrar Usuário")
     void postUsuario() {
         Ecommerce ecommerce = new Ecommerce();
         ecommerce.setIdEcommerce(6);
@@ -63,7 +81,8 @@ class UsuarioControllerTest {
     }
 
     @Test
-    void deleteUsuario() {
+    @DisplayName("DELETE /{idUsuario} - Deletar usuário por um id existente")
+    void deleteUsuarioOk() {
         int idTeste = 41;
 
         Mockito.when(usuarioRepository.existsById(idTeste)).thenReturn(true);
@@ -74,6 +93,23 @@ class UsuarioControllerTest {
     }
 
     @Test
+    @DisplayName("DELETE /{idUsuario} - Deletar usuário por um id não existente")
+    void deleteUsuarioNaoOk() {
+        int idTeste = 41;
+
+        Mockito.when(usuarioRepository.existsById(idTeste)).thenReturn(false);
+
+        try {
+            ResponseEntity resposta = usuarioController.deleteUsuario(idTeste);
+        } catch (ApiRequestException e) {
+            assertEquals(404, e.getStatus().value());
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("PUT /{idUsuario} - Quando não há Metas na base")
     void putUsuario() {
         int idTeste = 31;
         Ecommerce ecommerce = new Ecommerce();
