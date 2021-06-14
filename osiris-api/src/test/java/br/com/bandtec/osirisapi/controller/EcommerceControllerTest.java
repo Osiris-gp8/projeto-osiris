f6db3e0b3b7618f242edabb661da86fecf343923
Kaio Silva Baleeiro de Jesus <kaio.jesus@safra.com.br>
2021-06-14 07:19:29 -0300

    Testes dos ecommerces

--- osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/EcommerceControllerTest.java
+++ osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/EcommerceControllerTest.java
@@ -1,7 +1,9 @@
 package br.com.bandtec.osirisapi.controller;
 
 import br.com.bandtec.osirisapi.domain.Ecommerce;
+import br.com.bandtec.osirisapi.exception.ApiRequestException;
 import br.com.bandtec.osirisapi.repository.EcommerceRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -25,7 +28,8 @@ class EcommerceControllerTest {
     EcommerceRepository ecommerceRepository;
 
     @Test
-    void getEcommerce() {
+    @DisplayName("GET / - Quando há ecommerces na base")
+    void getEcommerceOk() {
         List<Ecommerce> ecommerceList =
                 Arrays.asList(new Ecommerce(), new Ecommerce(), new Ecommerce());
 
@@ -36,6 +40,20 @@ class EcommerceControllerTest {
     }
 
     @Test
+    @DisplayName("GET / - Quando há ecommerces na base")
+    void getEcommerceNaoOk() {
+        Mockito.when(ecommerceRepository.findAll()).thenReturn(new ArrayList<>());
+
+        try {
+            ResponseEntity<List<Ecommerce>> resposta = ecommerceController.getEcommerce();
+        } catch (ApiRequestException e) {
+            assertEquals(204, e.getStatus().value());
+        }
+
+    }
+
+    @Test
+    @DisplayName("POST / - Cadastrar um ecommerce")
     void postEcommerce() {
         Ecommerce ecommerce = new Ecommerce();
 
@@ -45,7 +63,8 @@ class EcommerceControllerTest {
     }
 
     @Test
-    void deleteEcommerce() {
+    @DisplayName("DELETE /{id} - Deletar um ecommerce por um id existente")
+    void deleteEcommerceOk() {
         int idTeste = 1;
 
         Mockito.when(ecommerceRepository.existsById(idTeste))
@@ -58,6 +77,24 @@ class EcommerceControllerTest {
     }
 
     @Test
+    @DisplayName("DELETE /{idEcommerce} - Deletar um ecommerce por um id não existente")
+    void deleteEcommerceNaoOk() {
+        int idTeste = 1;
+
+        Mockito.when(ecommerceRepository.existsById(idTeste))
+                .thenReturn(false);
+
+        try {
+            ResponseEntity resposta =
+                ecommerceController.deleteEcommerce(idTeste);
+        } catch (ApiRequestException e) {
+            assertEquals(404, e.getStatus().value());
+        }
+
+    }
+
+    @Test
+    @DisplayName("PUT /{idEcommerce} - Atualizar um ecommerce")
     void atualizarEcommerce() {
         int idTeste = 31;
         Ecommerce ecommerce = new Ecommerce();
