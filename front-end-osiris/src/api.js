316b100754077ab76da62cb70592d2cfc4091b1b
kaio-baleeiro <61800714+kaio-baleeiro@users.noreply.github.com>
2021-09-11 12:08:22 -0300

    Update api.js

--- front-end-osiris/src/api.js
+++ front-end-osiris/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://54.175.15.172:8080",
     headers: {
         "Access-Control-Allow-Origin": "http://localhost:8080",
         "Access-Control-Allow-Headers": "Authorization",
