b40208a4d7ddf8fa606d9cc31ee02d17c8c42dd5
kaio-baleeiro <61800714+kaio-baleeiro@users.noreply.github.com>
2021-09-13 17:52:45 -0300

    Update api.js

--- front-end-osiris/src/api.js
+++ front-end-osiris/src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "osiris-network:8080",
+    baseURL: "osiris-api:8080",
     headers: {
-        "Access-Control-Allow-Origin": "osiris-network:8080",
+        "Access-Control-Allow-Origin": "osiris-api:8080",
         "Access-Control-Allow-Headers": "Authorization",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
         "Authorization": `${sessionStorage.getItem("tipo")} ${sessionStorage.getItem("token")}`
