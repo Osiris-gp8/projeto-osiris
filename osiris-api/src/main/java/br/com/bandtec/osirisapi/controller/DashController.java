803c6558829d5b0cc49cadf25c4a00398921f8a1
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-09-20 21:22:34 -0300

    fix: removendo comentario

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/controller/DashController.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/controller/DashController.java
@@ -4,6 +4,7 @@ import br.com.bandtec.osirisapi.dto.request.FiltroDataRequest;
 import br.com.bandtec.osirisapi.dto.response.dash.AcessosVendasDiasResponse;
 import br.com.bandtec.osirisapi.service.DashService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,9 @@ public class DashController {
     private DashService dashService;
 
     @GetMapping("/contagem-acessos-vendas")
-    public List<AcessosVendasDiasResponse> getAcessosVendasUltimosSeteDias(FiltroDataRequest filtroDataRequest){
+    public ResponseEntity<List<AcessosVendasDiasResponse>> getAcessosVendasUltimosSeteDias(FiltroDataRequest filtroDataRequest){
 
-        return dashService.buscarAcessosVendas(filtroDataRequest);
+        return ResponseEntity.status(200).body(dashService.buscarAcessosVendas(filtroDataRequest));
     }
 
 }
