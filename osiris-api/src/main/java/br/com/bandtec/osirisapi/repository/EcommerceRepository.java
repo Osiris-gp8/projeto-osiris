583e0374aeff0e2e4ea89da2c79eb0c358330ad6
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-09-22 21:13:22 -0300

    Criando método de busca do id do ecommerce

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/repository/EcommerceRepository.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/repository/EcommerceRepository.java
@@ -2,6 +2,11 @@ package br.com.bandtec.osirisapi.repository;
 
 import br.com.bandtec.osirisapi.domain.Ecommerce;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EcommerceRepository extends JpaRepository<Ecommerce, Integer> {
+//    @Query(value = "select idEcommerce from evento where cnpj = ? and nome = ?", nativeQuery = true)
+//    int findEcommerceByCnpjEqualsAndNomeEquals();
+
+    int findEcommerceByCnpjEqualsAndNomeEquals(String cnpj, String nomeEcommerce);
 }
