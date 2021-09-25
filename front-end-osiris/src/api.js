5ceff149514d4c3f6dc5f8f1737abef743b5190d
daniel-zacarias <you@example.com>
2021-09-24 22:07:31 -0300

    trocando portas de loadbalancer

--- front-end-osiris/src/api.js
+++ front-end-osiris/src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://osiris-network:8080",
+    baseURL: "http://osiris-network:85",
     headers: {
-        "Access-Control-Allow-Origin": "http://osiris-network:8080",
+        "Access-Control-Allow-Origin": "http://osiris-network:85",
         "Access-Control-Allow-Headers": "Authorization",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
         "Authorization": `${sessionStorage.getItem("tipo")} ${sessionStorage.getItem("token")}`
