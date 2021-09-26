af3efb380af172daba5595de738b45c6ecbac38b
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-09-25 22:15:19 -0300

    Criando a classe de teste da autenticação

---
+++ osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,12 @@
+package br.com.bandtec.osirisapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutenticacaoControllerTest {
+
+    @Test
+    void autenticar() {
+    }
+}
