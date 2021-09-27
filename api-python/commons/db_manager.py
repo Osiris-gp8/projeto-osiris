37896be583919e741ad93c89655950837ba1d2b8
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-09-26 21:43:13 -0300

    feat (WIP): add connection to azure database

--- api-python/commons/db_manager.py
+++ api-python/commons/db_manager.py
@@ -1,20 +1,44 @@
+from enum import Enum
 from sqlalchemy import create_engine
 import pandas as pd
+import pyodbc
+
+class DbType(Enum):
+    MYSQL = 'mysql+mysqlconnector://{username}:{password}@{host}/{db}'
+    MSSQL = ('Driver={{ODBC Driver 13 for SQL Server}};'
+            'Server={host};'
+            'Database={db};'
+            'UID={username};'
+            'PWD={password};'
+            'Trusted_Connection=yes;')
+
 class DbManager:
+    def __init__(self, username, password, host, db, type: DbType):
+        self.type = type
+        self.connection_str = self.type.value.format(
+            username = username, password = password,
+            db = db, host= host
+        )
 
-    def __init__(self, username, password, host, db):
-        self.connection_str = f'mysql+mysqlconnector://{username}:{password}@{host}/{db}'
-
+    def _create_connection(self):
+        if self.type is DbType.MYSQL:
+            return create_engine(self.connection_str)
+        elif self.type is DbType.MSSQL:
+            # FIXME conexão com mssql não está funcionando
+            return pyodbc.connect(self.connection_str)
+        
     def read(self, query):
-        engine = create_engine(self.connection_str)
+        con = self._create_connection()
         print('Conectado ao banco')
-        result = pd.read_sql(query, con = engine, parse_dates=["dataCompra"])
+        result = pd.read_sql(query, con = con, parse_dates=["dataCompra"])
         data_frame = pd.DataFrame(result)
         data_frame.columns = result.keys()
         return data_frame
 
     def clean_table(self, table):
+        # FIXME ajustar para novo modelo, usando ENUM 
         engine = create_engine(self.connection_str)
         print('Limpando banco de processamento')
         with engine.connect() as con:
             con.execute("DELETE FROM {}".format(table))
+            
--- api-python/main.py
+++ api-python/main.py
@@ -1,6 +1,6 @@
 from typing import List
 from pipelines.base_pipeline import Pipeline
-from commons.db_manager import DbManager
+from commons.db_manager import DbManager, DbType
 from commons.api_client import ApiClient
 from commons.utils import *
 from pipelines.acessos_pipeline import AcessosPipeline
@@ -8,7 +8,7 @@ from pipelines.eventos_pipeline import EventosPipeline
 from pipelines.cupons_pipeline import CuponsPipeline
 
 def main():
-    db = DbManager("root", "bandtec123", "localhost", "processamento_db")
+    db = DbManager("root", "bandtec123", "localhost", "processamento_db", DbType.MYSQL)
     api = ApiClient("http://localhost:8080")
     
     pipelines: List[Pipeline] = [
