aac08616c7f8c47bafb10a64a0856579955b7b70
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-09-29 23:21:30 -0300

    test: fixed tests to connect to mysql database

--- api-python/tests/test_connections.py
+++ api-python/tests/test_connections.py
@@ -12,19 +12,22 @@ def test_connection_api():
         pytest.fail(f"Connection to API failed: {ip}")
         
 def test_connection_local_database():
-    ip = "http://localhost:8080"
+    ip = "localhost:3306"
     user = "root"
     password = "bandtec"
-    db = "processamento_db"
+    db = "osiris"
     try:
-        connection = DbManager(ip, password, user, db, DbType.MYSQL)
+        connection = DbManager(
+            host = ip, password = password, username = user, db = db, type = DbType.MYSQL
+        )
+        connection.read("SELECT 1")
     except :
         print(traceback.format_exc())
         pytest.fail(f"Connection to database failed {ip, user, password, db}")
         
 def test_connection_remote_database():
     # Azure
-    
+    # ! Deprecated
     ip = "tcp:server-osiris.database.windows.net"
     user = "osiris-adm"
     password = "#Gfgrupo8"
