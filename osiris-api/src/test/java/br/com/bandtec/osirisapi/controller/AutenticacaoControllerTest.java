b4f4810b5e43b2a3f506f4207698d7d5be964f24
Kaio Silva Baleeiro de Jesus <kaio.jesus@safra.com.br>
2021-10-01 21:59:02 -0300

    Criando os testes unitários de autenticação de usuário

--- osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/AutenticacaoControllerTest.java
+++ osiris-api/src/test/java/br/com/bandtec/osirisapi/controller/AutenticacaoControllerTest.java
@@ -1,12 +1,89 @@
 package br.com.bandtec.osirisapi.controller;
 
+import java.net.URI;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 class AutenticacaoControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void autenticar() {
+    @DisplayName("POST /auth - Testando autenticação caso o login e senha estejam incorretos")
+    void autenticarLoginInvalidoESenhaInvalida() throws Exception {
+        URI path = new URI("/auth");
+        String json = "{\"login\": \"usuario inválido\", \"senha\": \"senha inválida\"}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(path)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(400)
+                );
+    }
+
+    @Test
+    @DisplayName("POST /auth - Testando autenticação caso o login esteja correto e senha esteja incorreta")
+    void autenticarLoginCorretoESenhaInvalida() throws Exception {
+        URI path = new URI("/auth");
+        String json = "{\"login\": \"user8\", \"senha\": \"senha inválida\"}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(path)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(400)
+                );
+    }
+
+    @Test
+    @DisplayName("POST /auth - Testando autenticação caso o login esteja incorreto e senha esteja correta")
+    void autenticarLoginInvalidoESenhaCorreta() throws Exception {
+        URI path = new URI("/auth");
+        String json = "{\"login\": \"usuario inválido\", \"senha\": \"user8\"}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(path)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(400)
+                );
+    }
+
+    @Test
+    @DisplayName("POST /auth - Testando autenticação caso o login e senha esteja incorreta")
+    void autenticarLoginCorretoESenhaCorreta() throws Exception {
+        URI path = new URI("/auth");
+        String json = "{\"login\": \"user8\", \"senha\": \"user8\"}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(path)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(200)
+                );
     }
 }
