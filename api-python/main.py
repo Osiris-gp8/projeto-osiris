97372406d3c89183cb12c4ad9bb517a476e981b4
daniel-zacarias <you@example.com>
2021-10-02 21:44:33 -0300

    feat(Database) - Subindo Scripts de insert de AWS.

--- api-python/main.py
+++ api-python/main.py
@@ -1,7 +1,6 @@
 from typing import List
 from pipelines.base_pipeline import Pipeline
 from commons.db_manager import DbManager, DbType
-from commons.api_client import ApiClient
 from commons.utils import *
 from pipelines.acessos_pipeline import AcessosPipeline
 from pipelines.eventos_pipeline import EventosPipeline
@@ -9,11 +8,10 @@ from pipelines.cupons_pipeline import CuponsPipeline
 
 def main():
     db = DbManager("root", "bandtec", "localhost", "processamento_db", DbType.MYSQL)
-    api = ApiClient("http://localhost:8080")
     remote_db = DbManager("root", "bandtec", "3.233.173.254", "osiris", DbType.MYSQL)
     pipelines: List[Pipeline] = [
-        #AcessosPipeline(db, api), 
-        EventosPipeline(db, api, remote_db),
+        # AcessosPipeline(db, remote_db), 
+        EventosPipeline(db, remote_db),
         # CuponsPipeline()
     ]
     for p in pipelines:
