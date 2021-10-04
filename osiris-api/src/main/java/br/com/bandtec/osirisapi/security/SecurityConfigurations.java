e978f747276fc98ac31deac3132f4a37e09e59f2
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-10-04 19:10:23 -0300

    fix: Ajustes CORS

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/security/SecurityConfigurations.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/security/SecurityConfigurations.java
@@ -56,6 +56,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests().antMatchers("/auth").permitAll()
             .antMatchers(HttpMethod.POST,"/usuarios").permitAll()
+            .antMatchers("/usuarios/recuperar-senha/**").permitAll()
             .antMatchers(HttpMethod.GET, "/ecommerces/id").permitAll()
             .antMatchers(HttpMethod.POST, "/ecommerces").permitAll()
             .anyRequest().authenticated();
