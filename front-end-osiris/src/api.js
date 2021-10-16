e0985fadfd3460d91b905336ce7e32954133bb83
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-10-16 08:08:53 -0300

    alterando o endereço do backend

--- front-end-osiris/src/api.js
+++ front-end-osiris/src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://back-loadbalance:90",
+    baseURL: "http://localhost:8080",
     headers: {
-        "Access-Control-Allow-Origin": "http://back-loadbalance:90",
+        "Access-Control-Allow-Origin": "http://localhost:8080",
         "Access-Control-Allow-Headers": "Authorization",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
         "Authorization": `${sessionStorage.getItem("tipo")} ${sessionStorage.getItem("token")}`
