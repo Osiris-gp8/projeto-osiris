b1a0bb38593da4c9baefba2f960ec4da180a22da
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-10-16 08:23:57 -0300

    adaptando o build de imagens

--- web-server-build.sh
+++ web-server-build.sh
@@ -1,4 +1,2 @@
 docker build -f ./docker/frontend-nginx.dockerfile -t osiris/front-loadbalance .
-docker build -f ./docker/backend-nginx.dockerfile -t osiris/back-loadbalance .
 docker build -f ./docker/react.dockerfile -t osiris/frontend .
-docker build -f ./docker/springboot.dockerfile -t osiris/backend .
