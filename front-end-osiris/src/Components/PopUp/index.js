700e8ccffd1f4bdb397479fa7946f988cd1dd36f
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-11-08 20:34:32 -0300

    feat: calling osiris-api and handle error with toastfy

--- front-end-osiris/src/Components/PopUp/index.js
+++ front-end-osiris/src/Components/PopUp/index.js
@@ -1,12 +1,23 @@
 import download from '@iconify-icons/akar-icons/download';
 import Icon from '@iconify/react';
-import { useEffect } from 'react';
+import { getS3Files } from '../../services/uploadData';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import { toast, ToastContainer } from 'react-toastify';
+import { formatDateTime } from '../../services/utils'
+import { Link } from 'react-router-dom';
 
 
 export default () =>{ 
 
     const history = useHistory();
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [data, setData] = useState([])
+
+    const [headers, setHeaders] = useState({
+        "Authorization": `${sessionStorage.getItem("tipo")} ${sessionStorage.getItem("token")}`
+    });
 
     useEffect(() => {
         if(!sessionStorage.getItem("token")){
@@ -14,20 +25,40 @@ export default () =>{
         }
     }, [])
 
+    useEffect(() =>{
+        
+        
+        async function getData(){
+            const files = await getS3Files(headers, "2021-10-26T00:00:00")
+            setData(files)
+            setIsLoading(false)
+        }
+
+        getData()
+
+    }, [isLoading])
+
     return (
         <div>
             <table className="styledTable">
+                <thead>
                 <tr>
                     <th><h2>Nome</h2></th>
                     <th><h2>Data ultima alteração</h2></th>
                     <th><h2>Download</h2></th>
                 </tr>
-                <tr>
-                    <td>import 1</td>
-                    <td>08/11/2021</td>
-                    <td><Icon className="icon-menu" icon={download} /></td>
-                </tr>
+                </thead>
+                <tbody>
+                    {data.map((file) => 
+                        <tr key={file.httpRequest.allHeaders[2].value}>
+                            <td >{file.key}</td>
+                            <td>{file.metadata.lastModified}</td>
+                            <td><a href={file.httpRequest.uri}><Icon className="icon-menu" icon={download} /></a></td>
+                        </tr>)
+                    }
+                </tbody>
             </table>
+            <ToastContainer />
         </div>
     )
 }
