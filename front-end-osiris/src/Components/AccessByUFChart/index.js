9f04a28e74915256c94e781481260c055266cc0c
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-11-09 11:45:33 -0300

    feat: Adding files Page

--- front-end-osiris/src/Components/AccessByUFChart/index.js
+++ front-end-osiris/src/Components/AccessByUFChart/index.js
@@ -52,8 +52,3 @@ export function AccessByUFChart(){
         </>
     )
 }
-
-// [
-//     ["UF", "Acessos"],
-//     ["MT", 20]
-// ]
--- front-end-osiris/src/Components/PopUp/index.js
+++ front-end-osiris/src/Components/PopUp/index.js
@@ -3,9 +3,10 @@ import Icon from '@iconify/react';
 import { getS3Files } from '../../services/uploadData';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { formatDateTime } from '../../services/utils'
-import { Link } from 'react-router-dom';
 
 
 export default () =>{ 
@@ -25,21 +26,25 @@ export default () =>{
         }
     }, [])
 
-    useEffect(() =>{
-        
-        
-        async function getData(){
-            const files = await getS3Files(headers, "2021-10-26T00:00:00")
-            setData(files)
-            setIsLoading(false)
-        }
+    async function getData(date){
+        setIsLoading(true)
+        const files = await getS3Files(headers, date)
+        setData(files)
+        setIsLoading(false)
+    }
 
-        getData()
-
-    }, [isLoading])
+    function handleDate(e){
+        let date = e.target.value + "T00:00:00"
+        getData(date)
+    }
 
     return (
         <div>
+            <form style={{marginBottom: '15px'}}>
+                <label name="data" style={{marginBottom: '15px'}}>Selecione a data que subiu o arquivo: </label>
+                <br />
+                <input name="data" type="date" onChange={e => handleDate(e)} />
+            </form>
             <table className="styledTable">
                 <thead>
                 <tr>
@@ -52,12 +57,15 @@ export default () =>{
                     {data.map((file) => 
                         <tr key={file.httpRequest.allHeaders[2].value}>
                             <td >{file.key}</td>
-                            <td>{file.metadata.lastModified}</td>
-                            <td><a href={file.httpRequest.uri}><Icon className="icon-menu" icon={download} /></a></td>
+                            <td>{file.metadata.lastModified.slice(0, 10) + " as " + file.metadata.lastModified.slice(11, 19)}</td>
+                            <td><a href={file.httpRequest.uri}><Icon className="icon-menu" icon={download} />download</a></td>
                         </tr>)
                     }
                 </tbody>
             </table>
+            <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                {isLoading == true && <Loader type="Oval" color="#00BFFF" height={80} width={80}/>} 
+            </div>
             <ToastContainer />
         </div>
     )
