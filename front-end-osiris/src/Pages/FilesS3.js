147d9614eaaef6e2fe538da408c0a619c766122f
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-11-19 17:03:58 -0300

    fix: Ajustes de acordo com PR

--- front-end-osiris/src/Pages/FilesS3.js
+++ front-end-osiris/src/Pages/FilesS3.js
@@ -1,7 +1,7 @@
 import { useHistory } from "react-router";
 import { useEffect } from "react"; 
 import MenuNovo from "../Components/MenuNovo/MenuNovo";
-import PopUp from "../Components/PopUp"
+import FilesS3Table from "../Components/FilesS3"
 
 export default () => {
     
@@ -18,8 +18,8 @@ export default () => {
             <MenuNovo />
             <div className="body-config">
             <div className="container">
-                <h1>PopUp</h1>
-                <PopUp />
+                <h1>Files S3</h1>
+                <FilesS3Table />
             </div>
             </div>
         </div>
