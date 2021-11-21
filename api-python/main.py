dcfa1a7e1d14918a34158c87a8b5deb7474fae73
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-11-21 13:18:40 -0300

    build: changed database ip

--- api-python/main.py
+++ api-python/main.py
@@ -10,13 +10,13 @@ from pipelines.fill_cupons_pipeline import FillCupons
 
 def main():
     db = DbManager("admin", "bandtec", "127.0.0.1", "processamento_db", DbType.MYSQL)
-    remote_db = DbManager("root", "bandtec", "18.211.88.253", "osiris", DbType.MYSQL)
+    remote_db = DbManager("root", "bandtec", "3.94.235.84:3306", "osiris", DbType.MYSQL)
     pipelines: List[Pipeline] = [
         AcessosPipeline(db, remote_db), 
-        # EventosPipeline(db, remote_db),
-        # CuponsPipeline(remote_db),
-        # FillCupons(remote_db)
-        # MetasPipeline(db, remote_db)
+        EventosPipeline(db, remote_db),
+        CuponsPipeline(remote_db),
+        FillCupons(remote_db),
+        MetasPipeline(db, remote_db)
     ]
     for p in pipelines:
         p.run()
