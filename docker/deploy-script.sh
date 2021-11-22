0f648de0afc16b1364e580164b8c1d1d6b6c76dc
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-11-22 00:37:25 -0300

    Retirando deploy-script.sh

--- docker/deploy-script.sh
+++
@@ -1,7 +0,0 @@
-echo "$PRIVATE_KEY" > private_key && chmod 600 private_key
-
-scp -o StrictHostKeyChecking=no ./docker/docker-compose.yml -i private_key ${USER}@${HOSTNAME}:/home/ubuntu/docker-compose.yml
-
-ssh -o StrictHostKeyChecking=no -i private_key ${USER}@${HOSTNAME} '
-    docker-compose up -d --scale springboot=2
-'
