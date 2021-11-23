363be0448233abddfa5a9c7e8045512402919f89
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-11-22 23:38:38 -0300

    refactor: Removendo arquivo temporario

--- shell-script/arquivos-com-erro.sh
+++ shell-script/arquivos-com-erro.sh
@@ -7,11 +7,14 @@ then
 fi
 
 endereco=$1
+path_json_temp=./temp/arquivo.json
 
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
-curl $1 -o ./temp/arquivo.json
+curl $1 -o $path_json_temp
 
 contagem=$(cat ./temp/arquivo.json | awk -F: '{ print $2 }' | awk -F} '{ print $1 }')
+
+rm $path_json_temp
 
 /usr/bin/env aws cloudwatch put-metric-data --metric-name ArquivosComFalhas --namespace Custom --unit Count --value $contagem --dimensions InstanceId=$instance_id
