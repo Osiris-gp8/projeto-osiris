c8f9123509cac840c7890ec298bcafb45562dd36
Kaique Lucena <kaique.lucena@cerc.inf.br>
2021-11-22 23:32:46 -0300

    feat: Ajustes Shell Script contagem de arquivos enviados com falha

--- shell-script/arquivos-com-erro.sh
+++ shell-script/arquivos-com-erro.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ -z $1 ]
 then   
@@ -12,4 +12,6 @@ instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 curl $1 -o ./temp/arquivo.json
 
-echo $(cat ./temp/arquivo.txt)
+contagem=$(cat ./temp/arquivo.json | awk -F: '{ print $2 }' | awk -F} '{ print $1 }')
+
+/usr/bin/env aws cloudwatch put-metric-data --metric-name ArquivosComFalhas --namespace Custom --unit Count --value $contagem --dimensions InstanceId=$instance_id
