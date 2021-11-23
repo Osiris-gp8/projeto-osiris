dbe04a34ef6c57ff7eefc818d3c6c8b802242d9f
patrick_lessa <patrick.lessa03@outlook.com>
2021-11-22 21:12:54 -0300

    update: baseURL da API

--- front-end-osiris/src/api.js
+++ front-end-osiris/src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://backend-osiris.ddns.net",
     headers: {
-        "Access-Control-Allow-Origin": "http://localhost:8080",
+        "Access-Control-Allow-Origin": "http://backend-osiris.ddns.net",
         "Access-Control-Allow-Headers": "Authorization",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE"
     }
