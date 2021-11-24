11c7cbc26db4ae170b05e2322875895477d4d7b9
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2021-11-23 23:04:59 -0300

    Alterando endereços ip's e tirando scp

--- osiris-api/src/main/java/br/com/bandtec/osirisapi/mensageria/Constants.java
+++ osiris-api/src/main/java/br/com/bandtec/osirisapi/mensageria/Constants.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Constants {
 
-    private String frontUrl = "http://3.95.60.151";
+    private String frontUrl = "http://osiris-ti.ddns.net/";
 
     public String ASSUNTO_RECUPERAR_SENHA = "Recuperar senha";
     public String ASSUNTO_PRIMEIRO_LOGIN = "Osiris primerio login";
